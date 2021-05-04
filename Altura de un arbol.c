bd56c319808a228c87747ffafeb2b3cc5d7a1b33
AndresRH2002 <82550010+AndresRH2002@users.noreply.github.com>
2021-05-03 19:03:27 -0500

    Add files via upload

---
+++ Altura de un arbol.c
@@ -0,0 +1,15 @@
+//Para sacar la altura de un arbol si tiene una sombra de 17 cm de longitus
+#include stdio.h
+int main()
+{
+    int opcion;
+    float Longitud de la sombra, Tangente del angulo;
+    printf("Dame el valor de la Longitud de la sombra");
+    scanf("%f", &Longitud de la sombra);
+    printf("Dame el valor de la Tangente del angulo");
+    scanf("%f", &Tangente del angulo);
+
+    Altura = Tangente del angulo/Longitud de la sombra;
+    printf("El valor de la Altura es: %f, Altura");
+    return 0;
+}
---
+++ Peso de un objeto.c
@@ -0,0 +1,15 @@
+//Para sacar el peso de un objeto
+#include stdio.h
+int main()
+{
+    int opcion;
+    float Masa, Gravedad;
+    printf("Dame el valor de la Masa");
+    scanf("%f", &Masa);
+    printf("Dame el valor de la Gravedad");
+    scanf("%f", &Gravedad);
+
+    Peso = Masa*Gravedad
+    printf("El valor del Peso es: %f, Peso");
+    return 0;
+}
